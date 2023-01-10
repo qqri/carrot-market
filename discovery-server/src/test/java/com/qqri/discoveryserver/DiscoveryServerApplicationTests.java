a939cdeb778c24da79ec48eb05b860c39eb6fe2d
qqri <cqo1154@gmail.com>
2023-01-10 23:19:32 +0900

    add discovery-service project

---
+++ discovery-server/src/test/java/com/qqri/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqri.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
