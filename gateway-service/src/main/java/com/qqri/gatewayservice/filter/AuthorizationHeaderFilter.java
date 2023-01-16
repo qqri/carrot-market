aadda271fafe373cbe2682248bba1e85612c6569
qqri <cqo1154@gmail.com>
2023-01-16 22:32:20 +0900

    user-service | add security config

---
+++ gateway-service/src/main/java/com/qqri/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,73 @@
+package com.qqri.gatewayservice.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    public static class Config{
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String token = exchange.getRequest().getHeaders().get("Authorization").get().substring(7);
+            Map<String, Object> userInfo = jwtUtil.getUserParseInfo(token);
+
+            addAuthorizationHeaders(exchange.getRequest(), userInfo);
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private void addAuthorizationHeaders(ServerHttpRequest request, Map<String, Object> userInfo) {
+        request.mutate()
+                .header("X-Authorization-Id" , userInfo.get("memberId").toString())
+                .build();
+    }
+
+    @Bean
+    public ErrorWebExceptionHandler implements ErrorWebExceptionHandler {
+        private String getErrorCode(int errorCode) {
+            return "{\\"errorCode\\":" + errorCode + "}";
+
+        }
+
+        @Override
+        public Mono<Void> handle(
+                ServerWebExchange exchange, Throwable ex) {
+            int errorCode = 500;
+            if (ex.getClass() == NullPointerException.class) {
+                errorCode = 100;
+            } else if (ex.getClass() == ExpiredJwtException.class) {
+                errorCode = 200;
+            }
+
+            byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+            return exchange.getResponse().writeWith(Flux.just(buffer));
+
+        }
+    }
+}
--- user-service/src/main/java/com/qqri/userservice/controller/UserController.java
+++ user-service/src/main/java/com/qqri/userservice/controller/UserController.java
@@ -1,14 +1,47 @@
 package com.qqri.userservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.qqri.userservice.JwtTokenProvider;
+import com.qqri.userservice.domain.User;
+import com.qqri.userservice.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user-service")
+@RequiredArgsConstructor
 public class UserController {
     @GetMapping("/hello")
     public String user() {
         return "hi user-service~!! ";
+    }
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user) {
+        return userRepository.save(User.builder()
+                .name(user.get("name"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        User member = userRepository.findByName(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
     }
 }
